a55e0877494891c2af342b9b157afcb9fa0e0e9a
sam960905@gmail.com <rygbghersighsrujghksrug>
2020-05-26 22:38:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-// let D1 = new Date(),
+let D1 = new Date(),
+D1last = (D1.getFullYear(), D1.getMonth()+1,0).getDate(),
+D1Nlast = new Date(D1.getFullYear(),D1.getMonth(),D1last).getDay(),
+D1Nfirst = new Date(D1.getFullYear(),1).getDay(),
+calendar = '<tr>',
+month = ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
 
 // < keycode 37
 // > keycode 39
