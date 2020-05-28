6277eea48b44d92cc7883b894d9fd17896e29c21
sam960905@gmail.com <rygbghersighsrujghksrug>
2020-05-28 18:16:12 +0300

    Fix script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const date_picker_element = document.querySelector('.date-picker');
 const selected_date_element = document.querySelector('.date-picker .selected-date');
 const dates_element = document.querySelector('.date-picker .dates');
-const mth_element = document.querySelector('date-picker .dates .month .mth');
+const mth_element = document.querySelector('.date-picker .dates .month .mth');
 const next_mth_element = document.querySelector('date-picker .dates .month .next-mth');
 const prev_mth_element = document.querySelector('date-picker .dates .month .prev-mth');
 const days_element = document.querySelector('.date-picker .dates .days');
@@ -20,7 +20,8 @@ let selectedDay = day;
 let selectedMonth = month;
 let selectedYear = year;
 
-mth_element.textContent = months[month];
+mth_element.textContent = months[month] + ' ' + year;
+
 
 function toggleDatePicker(e) {
   if (!checkEventPathForClass(e.path, 'dates')) {
