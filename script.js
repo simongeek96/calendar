b8f554c43cd3844a2be7de523b322b2848a4d504
Simon <33905417+simongeek96@users.noreply.github.com>
2020-06-09 20:33:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const days_element = document.querySelector('.date-picker .dates .days');
 
 const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
 const daysOfWeek = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+const isWeekend = day => {
+  return day + `: ${day % 7}`;
+  // return day % 7===6 || day % 7 == 0;
+  
+}
 date_picker_element.addEventListener('click', toggleDatePicker);
 
 let date = new Date();
@@ -20,7 +25,11 @@ let year = date.getFullYear();
 // let lastDay = new Date(date.getFullYear(), date.getMonth()+1,0).getDate;
 // let WlastDay = new Date(date.getFullYear(), date.getMonth(),lastDay).getDay;
 // let WfirstDay = new Date(date.getFullYear(), date.getMonth(),1).getDay;
-let firstDay = [7, 1, 2, 3, 4, 5, 6][new Date(year, month, 1).getDay()];
+// let firstDay = [7, 1, 2, 3, 4, 5, 6][new Date(year, month, 1).getDay()];
+// for (let day = 1; day <= 31; day++) {
+//   const weekend = isWeekend(day)
+//   console.log(day);
+// }
 
 let selectedDate = date;
 let selectedDay = day;
@@ -67,13 +76,27 @@ function goToPrevMonth(e) { //переключение на пред. месяц
   populateDates();
 }
 
-function populateDates (e) {  //отрисовка дат
+function populateDates (e) {  //отрисовка калкндаря
   days_element.innerHTML = '';
   const amount_days = daysInMonth(month, year);
-
+  
   function daysInMonth(month, year) {
     return new Date(year, month + 1, 0).getDate();
  }
+
+  // let monthDays = new Date (year, month + 1, 0).getDate(),
+  //     monthPrefix = new Date(year, month, 0).getDay(),
+  //     monthDaysText = '';
+
+  //   if (monthPrefix > 0) {
+  //     for (let i = 1; i <= monthPrefix; i++) {
+  //         monthDaysText = day_element.classList.add('day');
+  //     }
+  //   }
+
+  //   for (let i = 1; i <= monthDays; i++){
+  //     monthDaysText = day_element.classList.add('day');
+  //   }
 
   for (let i = 0; i < amount_days; i++) {
     const day_element = document.createElement('div');
@@ -83,6 +106,11 @@ function populateDates (e) {  //отрисовка дат
     if (selectedDay == (i + 1) && selectedYear == year && selectedMonth == month) {
       days_element.classList.add('selected');
     }
+
+    // function getWeeks(year, month) {
+    //   let l = new Date (year, month+1, 0);
+    //   return Math.ceil((l.getDate() - (l.getDay()?l.getDay():7))/7)+1;
+    // }
 
     day_element.addEventListener('click', function () { //выбор определённой даты
         selectedDate = new Date(year + '-' + (month + 1) + '-' + (i + 1));
